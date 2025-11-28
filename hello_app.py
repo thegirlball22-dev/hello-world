72c45bb00b51bcf44452d8a77d77eb59e9bb16ab
thegirlball22-dev <thegirlball22@gmail.com>
2025-11-27 23:32:20 -0600

    Create hello_app.py

---
+++ hello_app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello from Render! Deployment successful!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
